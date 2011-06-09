ba810850598653b71ad1d53301dd8556eb5982f3
Ryan Jafari <ryan@cetrasoft.com>
2011-06-09 17:36:59 -0400

    analytics, meetup group, caching line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :cache_page
-  
-  protected
-  
-  def cache_page
-    unless Rails.env.development?
-      expires_in 1.year, :public => true
-    end
+  def show
+    response.headers['Cache-Control'] = 'public, max-age=525600'
   end
+
 end
