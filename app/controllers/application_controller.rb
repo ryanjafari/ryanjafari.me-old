03cf40c1ac479803f9e6fd2fe08a872b24f5625b
Ryan Jafari <ryan@cetrasoft.com>
2013-05-15 22:14:27 -0400

    Redirect to ryanandcarlos.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def show
+    redirect_to 'http://ryanandcarlos.com'
     response.headers['Cache-Control'] = 'public, max-age=525600'
   end
 
